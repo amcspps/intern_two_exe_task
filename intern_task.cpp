4876b6973edc5696a360a49b20fb9968fdb8356f
kchnms <pashafizmat1@gmail.com>
2023-02-09 17:35:52 +0300

    cmakelists

---
+++ intern_task.cpp
@@ -0,0 +1,12 @@
+﻿// intern_task.cpp: определяет точку входа для приложения.
+//
+
+#include "intern_task.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
